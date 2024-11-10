79cfe424d04fedf4f94fc295a6dc40ad6f6fe02d
Min. <154395301+camilasant-pvz5@users.noreply.github.com>
2024-11-10 15:32:58 -0300

    sobre carrito, funcionalidad

--- proyectofinal/src/context/CartContext.jsx
+++ proyectofinal/src/context/CartContext.jsx
@@ -9,6 +9,41 @@ export const CartProvider = ({ children }) => {
   
   const removeFromCart = (productId) => 
     setCart(cart.filter(item => item.id !== productId));
+
+/* Se agrega el updateQuantity para que el carrito sume o reste */
+
+  const updateQuantity = (productId, quantity) => {
+    setCart(cart.map(item => 
+      item.id === productId ? { ...item, cantidad: quantity } : item
+    ));
+  };
+
+  const clearCart = () => setCart([]);
+
+  return (
+    <CartContext.Provider value={{ 
+      cart, 
+      addToCart, 
+      removeFromCart,
+      clearCart,
+      updateQuantity 
+    }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+/* 
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => setCart([...cart, product]);
+  
+  const removeFromCart = (productId) => 
+    setCart(cart.filter(item => item.id !== productId));
   
 
   const clearCart = () => setCart([]);
@@ -23,4 +58,4 @@ export const CartProvider = ({ children }) => {
       {children}
     </CartContext.Provider>
   );
-};
+}; */
